795d3d8fabab413a3fba88f3f3dccab4cdd1511e
jgmejiaco <jgmejiaco@gmail.com>
2022-09-16 20:22:36 -0500

    menu con while

---
+++ main.py
@@ -0,0 +1,11 @@
+contador = 5
+
+while(contador != 5):
+    print("MENÚ")
+    print("*****************")
+    print("1. Recibir nombre y año de nacimiento")
+    print("2. calcular edad y mostrar en pantalla")
+    print("3. mostrar saludo de buenas noches al usuario")
+    print("4. contarle cuantos años tendra en el 2030")
+    print("5. SALIR")
+    print("*****************")
